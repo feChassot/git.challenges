a385e8fa032a46a0013990f8cea28f2ca7a80521
Felipe Chassot <fechassotdev@gmail.com>
2025-03-14 15:31:11 -0300

    Teste v1.0

---
+++ FirstGitProject.java
@@ -0,0 +1,17 @@
+package git.challenges;
+
+import java.util.Scanner;
+
+public class FirstGitProject {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Valor teste: ");
+        double teste = sc.nextDouble();
+
+        double calc = teste * 2.0;
+
+        System.out.println("Valor total: " + calc);
+    }
+
+}
