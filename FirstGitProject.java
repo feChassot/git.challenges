de157e28c07ac8de9393c211ce7f50e4672d92e8
Felipe Chassot <fechassotdev@gmail.com>
2025-03-14 15:33:15 -0300

    Teste v3.0

--- FirstGitProject.java
+++ FirstGitProject.java
@@ -1,9 +1,11 @@
 package git.challenges;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class FirstGitProject {
     public static void main (String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Valor teste: ");
